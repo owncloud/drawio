4a01fb4a6252eb0dd880578aa12716729d2a2805
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2023-05-05 11:03:08 +0200

    feat: lock the file to prevent data loss

--- js/editor.js
+++ js/editor.js
@@ -25,87 +25,106 @@
 
 	if (!OCA.Drawio.AppName) {
 		OCA.Drawio = {
-			AppName: "drawio"
+			AppName: "drawio",
+			currentLockToken: null
 		};
 	}
 
-	OCA.Drawio.LoadEditorHandler = function(eventHandler, path, editWindow) {
+	function getHrefNodeContents (node) {
+		var nodes = node.getElementsByTagNameNS(OC.Files.Client.NS_DAV, 'href');
+		if (!nodes.length) {
+			return null;
+		}
+		return nodes[0].textContent;
+	}
+
+	OCA.Drawio.LoadEditorHandler = function (eventHandler, path, editWindow) {
 		// Set page title for webbrowser tab window
 		window.document.title = path.split("/").pop() + ' - ' + oc_defaults.title;
 		// Handle the load event at the start of the page load
 		var loadMsg = OC.Notification.show(t(OCA.Drawio.AppName, "Loading diagram..."));
 		var fileClient = OC.Files.getClient();
-		fileClient.getFileContents(path)
+		fileClient.lock(path)
 			.then(function (status, contents) {
-				// If the file is empty, then we start with a new file template
-				if (contents === "") {
-					editWindow.postMessage(JSON.stringify({
-						action: "template",
-						name: path
-					}), "*");
-				} else if (contents.indexOf("mxGraphModel") === -1) {
-					// If the contents is something else, we just error and exit
-					OC.Notification.show(t(OCA.Drawio.AppName, "Error: This is not a Drawio file!"));
-				} else {
-					// Load the xml from the file and setup drawio
-					editWindow.postMessage(JSON.stringify({
-						action: "load",
-						xml: contents
-					}), "*");
-				}
-			})
-			// Loading failed
-			.fail(function (status) {
-				OC.Notification.show(t(OCA.Drawio.AppName, "Error: Failed to load the file!"));
-			})
-			// Loading done, hide message
-			.done(function () {
-				OC.Notification.hide(loadMsg);
-			});
-	};
+				const xml = contents.xhr.responseXML;
+				const activelock = xml.getElementsByTagNameNS('DAV:', 'activelock');
 
-	OCA.Drawio.SaveFileHandler = function(eventHandler, path, payload) {
-		// Handle the save event triggered by the user, use JS to save over WebDAV
-		var saveNotif = OC.Notification.show(t(OCA.Drawio.AppName, "Saving..."));
-		var fileClient = OC.Files.getClient();
-		fileClient.putFileContents(
-			path,
-			payload.xml, {
-				contentType: "x-application/drawio",
-				overwrite: false
-			}
-		)
-		// After save, show nice message
-		.then(function () {
-			OC.Notification.showTemporary(t(OCA.Drawio.AppName, "Saved"));
-		})
-		// Saving failed
-		.fail(function () {
-			OC.Notification.show(t(OCA.Drawio.AppName, "Error: Could not save file!"));
-		})
-		// Saving is done, hide original message
-		.done(function () {
-			OC.Notification.hide(saveNotif);
-		});
-	};
+				OCA.Drawio.currentLockToken = getHrefNodeContents(activelock[0].getElementsByTagNameNS('DAV:', 'locktoken')[0])
 
-	OCA.Drawio.ExitHandler = function(eventHandler, path) {
-		// Stop listening
-		window.removeEventListener("message", eventHandler);
-		window.close();
-		// If this doesn't work, fallback to opening the files app instead.
-		OC.Files.getClient().getFileInfo(path)
-			.then(function (status, fileInfo) {
-				window.location.href = OC.generateUrl(
-					"/apps/files/?dir={currentDirectory}&fileid={fileId}",
-					{
-						currentDirectory: fileInfo.path,
-						fileId: fileInfo.id
+				fileClient.getFileContents(path)
+					.then(function (status, contents) {
+						// If the file is empty, then we start with a new file template
+						if (contents === "") {
+							editWindow.postMessage(JSON.stringify({
+								action: "template",
+								name: path
+							}), "*");
+						} else if (contents.indexOf("mxGraphModel") === -1) {
+							// If the contents is something else, we just error and exit
+							OC.Notification.show(t(OCA.Drawio.AppName, "Error: This is not a Drawio file!"));
+						} else {
+							// Load the xml from the file and setup drawio
+							editWindow.postMessage(JSON.stringify({
+								action: "load",
+								xml: contents
+							}), "*");
+						}
+					})
+					// Loading failed
+					.fail(function () {
+						OC.Notification.show(t(OCA.Drawio.AppName, "Error: Failed to load the file!"));
+					})
+					// Loading done, hide message
+					.done(function () {
+						OC.Notification.hide(loadMsg);
 					});
 			})
 			.fail(function () {
-				window.location.href = OC.generateUrl("/apps/files");
+				OC.Notification.show(t(OCA.Drawio.AppName, "File is currently being used by somebody else"));
+			})
+	};
+
+	OCA.Drawio.SaveFileHandler = function (eventHandler, path, payload) {
+		// Handle the save event triggered by the user, use JS to save over WebDAV
+		var saveNotif = OC.Notification.show(t(OCA.Drawio.AppName, "Saving..."));
+		OCA.Drawio.putFileContents(
+			path,
+			payload.xml
+		)
+			// After save, show nice message
+			.then(function () {
+				OC.Notification.showTemporary(t(OCA.Drawio.AppName, "Saved"));
+			})
+			// Saving failed
+			.fail(function () {
+				OC.Notification.show(t(OCA.Drawio.AppName, "Error: Could not save file!"));
+			})
+			// Saving is done, hide original message
+			.done(function () {
+				OC.Notification.hide(saveNotif);
 			});
+	};
+
+	OCA.Drawio.ExitHandler = function (eventHandler, path) {
+		OCA.Drawio.unlock(path, OCA.Drawio.currentLockToken)
+			.done(function () {
+				// Stop listening
+				window.removeEventListener("message", eventHandler);
+				window.close();
+				// If this doesn't work, fallback to opening the files app instead.
+				OC.Files.getClient().getFileInfo(path)
+					.then(function (status, fileInfo) {
+						window.location.href = OC.generateUrl(
+							"/apps/files/?dir={currentDirectory}&fileid={fileId}",
+							{
+								currentDirectory: fileInfo.path,
+								fileId: fileInfo.id
+							});
+					})
+					.fail(function () {
+						window.location.href = OC.generateUrl("/apps/files");
+					});
+			})
 	};
 
 	OCA.Drawio.LoadEventHandler = function (editWindow, path, origin) {
@@ -123,4 +142,55 @@
 		};
 		window.addEventListener("message", eventHandler);
 	};
+
+	OCA.Drawio.unlock = function (path, token) {
+		var fileClient = OC.Files.getClient();
+		var deferred = $.Deferred();
+		var promise = deferred.promise();
+
+		fileClient.getClient().request('UNLOCK',
+			fileClient._buildUrl(path),
+			{
+				'Lock-Token': token
+			}
+		).then(
+			function(result) {
+				if (fileClient._isSuccessStatus(result.status)) {
+					deferred.resolve(result.status, result);
+				} else {
+					result = _.extend(result, fileClient._getSabreException(result));
+					deferred.reject(result.status, result);
+				}
+			}
+		);
+		return promise;
+	}
+
+	OCA.Drawio.putFileContents = function (path, body) {
+		var fileClient = OC.Files.getClient();
+		var headers = {
+			'Content-Type': "x-application/drawio",
+			'If': '(<' + OCA.Drawio.currentLockToken + '>)'
+		}
+
+		var deferred = $.Deferred();
+		var promise = deferred.promise();
+
+		fileClient.getClient().request(
+			'PUT',
+			fileClient._buildUrl(path),
+			headers,
+			body
+		).then(
+			function(result) {
+				if (fileClient._isSuccessStatus(result.status)) {
+					deferred.resolve(result.status, result);
+				} else {
+					result = _.extend(result, fileClient._getSabreException(result));
+					deferred.reject(result.status, result);
+				}
+			}
+		);
+		return promise;
+	}
 })(OCA);
