0387bb99f25020f80ea437651f480dcd9d92a0b7
ownClouders <devops@owncloud.com>
2024-02-10 00:35:12 +0000

    [tx] updated from transifex

---
+++ l10n/ja.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "drawio",
+    {
+    "Loading diagram..." : "図を読み込んでいます...",
+    "Error: This is not a Drawio file!" : "エラー: これは Drawio ファイルではありません。",
+    "Error: Failed to load the file!" : "エラー: ファイルのロードに失敗しました。",
+    "File is currently being used by somebody else" : "ファイルは現在他の人によって使用されています",
+    "Saving..." : "保存中...",
+    "Saved" : "保存されました",
+    "Error: Could not save file!" : "エラー: ファイルを保存できませんでした。",
+    "Failed to create a new file." : "新しいファイルの作成に失敗しました。",
+    "Open in Draw.io" : "Draw.ioで開く",
+    "Diagram" : "ダイアグラム",
+    "New Diagram.drawio" : "新しい図.drawio",
+    "There was an error loading the diagram file. Check permissions and try again." : "ダイアグラムファイルの読み込みエラーが発生しました。ファイルアクセス権限を確認し、再度実行してください。"
+},
+"nplurals=1; plural=0;");
